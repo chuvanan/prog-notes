b9293c0a0022d6a41198004be9e5f46a688e8888
An Chu <chuvanan.cva@gmail.com>
2018-06-04 15:51:15 +0700

    Update test cases

--- OCR/ocr.R
+++ OCR/ocr.R
@@ -25,29 +25,54 @@ pdf_files <- get_pdfs(path = "~/Dropbox/prog-notes/OCR/")
 
 ## Output:
 ## [1] "/home/anchu/Dropbox/prog-notes/OCR/G-000100.pdf"
+## [1] "/home/anchu/Dropbox/prog-notes/OCR/G-000157.pdf"
+## [1] "/home/anchu/Dropbox/prog-notes/OCR/G-000174.pdf"
 
 pdf2png(pdf_files)
 ## Output:
 ## Converting page 1 to G-000100_1.png... done!
 ## Converting page 2 to G-000100_2.png... done!
+## Converting page 1 to G-000157_1.png... done!
+## Converting page 2 to G-000157_2.png... done!
+## Converting page 1 to G-000174_1.png... done!
+## Converting page 2 to G-000174_2.png... done!
 
 ## -----------------------------------------------------------------------------
 ## Image pre-processing
 
-png1 <- image_read("~/Dropbox/prog-notes/OCR/G-000100_1.png")
-png2 <- image_read("~/Dropbox/prog-notes/OCR/G-000100_2.png")
+png100_1 <- image_read("~/Dropbox/prog-notes/OCR/G-000100_1.png")
+png100_2 <- image_read("~/Dropbox/prog-notes/OCR/G-000100_2.png")
+png157_1 <- image_read("~/Dropbox/prog-notes/OCR/G-000157_1.png")
+png157_2 <- image_read("~/Dropbox/prog-notes/OCR/G-000157_2.png")
+png174_1 <- image_read("~/Dropbox/prog-notes/OCR/G-000174_1.png")
+png174_2 <- image_read("~/Dropbox/prog-notes/OCR/G-000174_2.png")
 
-png1 <- image_crop(png1, "3000x2500+60+2100")
-png2 <- image_crop(png2, "3000x3000+60+70")
+
+png100_1 <- image_crop(png100_1, "3000x2500+60+2100")
+png100_2 <- image_crop(png100_2, "3000x3000+60+70")
 png2 <- image_rotate(png2, 1)
-image_display(png2, F)
 
+png157_1 <- image_crop(png157_1, "3000x2500+60+2100")
+png157_2 <- image_crop(png157_2, "3000x3000+60+70")
+
+png174_1 <- image_crop(png174_1, "3000x2500+60+2100")
+png174_2 <- image_crop(png174_2, "3000x3000+60+70")
 
 ## -----------------------------------------------------------------------------
 ## Extract text
 
 vie <- tesseract::tesseract("vie")
-text1 <- ocr(png1, engine = vie)
-text2 <- ocr(png2, engine = vie)
-cat(text1)
-cat(text2)
+text100_1 <- ocr(png100_1, engine = vie)
+text100_2 <- ocr(png100_2, engine = vie)
+cat(text100_1)
+cat(text100_2)
+
+text157_1 <- ocr(png157_1, engine = vie)
+text157_2 <- ocr(png157_2, engine = vie)
+cat(text157_1)
+cat(text157_2)
+
+text174_1 <- ocr(png174_1, engine = vie)
+text174_2 <- ocr(png174_2, engine = vie)
+cat(text174_1)
+cat(text174_2)
