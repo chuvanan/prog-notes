642ded330e2ff326ee26f1f996e908c01aa8a245
An Chu <chuvanan.cva@gmail.com>
2019-11-15 21:23:01 +0700

    Clang-fotmat is great

--- Rccp/cpp11-features.cpp
+++ Rccp/cpp11-features.cpp
@@ -7,20 +7,18 @@ NumericVector create_vector() {
 
     NumericVector v1 = NumericVector::create(1.0, 2.0, 3.0);
     NumericVector v2 = {1.0, 2.0, 3.0};
-    NumericVector v3 {1.0, 2.0, 3.0};
+    NumericVector v3{1.0, 2.0, 3.0};
 
-    return(v1 + v2 + v3);
+    return (v1 + v2 + v3);
 }
 
 // [[Rcpp::export]]
 NumericVector rcpp_lambda(NumericVector v) {
     /* NumericVector v = {1, 2, 3, 4, 5}; */
     double a = 2.0;
-    NumericVector res = sapply(v, [&](double x){return a * x; });
-    return(res);
+    NumericVector res = sapply(v, [&](double x) { return a * x; });
+    return (res);
 }
 
 // [[Rcpp::export]]
-void rcpp_rcout(NumericVector x) {
-    Rcout << "The value of x: " << x << "\n";
-}
+void rcpp_rcout(NumericVector x) { Rcout << "The value of x: " << x << "\n"; }
